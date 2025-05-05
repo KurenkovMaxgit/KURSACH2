2f020c940926f27369a94364ec9ea1611b4d4057
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2025-05-05 18:22:13 +0300

    Footer deleted & About photo formatted

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -3,10 +3,10 @@
     <h1 class="mb-2">Група: ІС-33</h1>
     <h1 class="mb-2">Виконавець: Куренков Максим Володимирович</h1>
     <div v-if="rageMode">
-        <img src="/TF_is_a_kilometer_RAGE.jpg" alt="rage" width="800" height="900">
+        <img src="/TF_is_a_kilometer_RAGE.jpg" alt="rage" style="width: 100%; height: 100%;">
     </div>
     <div v-else>
-        <img src="/TF_is_a_kilometer.jpg" alt="normal" width="800" height="900">
+        <img src="/TF_is_a_kilometer.jpg" alt="normal" style="width: 100%; height: 100%;">
     </div>
     <v-switch label="RAGE MODE" v-model="rageMode" inset></v-switch>
 </template>
